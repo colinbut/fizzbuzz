6fabb74918e0dfba83362a42a543754629be7deb
Colin But <colin.but@hotmail.co.uk>
2015-02-14 22:38:32 +0000

    Added fizzbuzz code

---
+++ src/main/java/com/mycompany/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.mycompany.fizzbuzz;
+
+/**
+ * The ever class FizzBuzz problem
+ * 
+ * @author Colin
+ *
+ */
+public class FizzBuzz {
+
+
+	public static void fizzBuzzV1(){
+		
+		for(int i = 1; i <= 100; i++)
+		{
+		
+			if((i % 3 == 0) && (i % 5 == 0)){
+				System.out.println("FizzBuzz " + i);
+			}
+			else if((i % 3) == 0){
+				System.out.println("Fizz " + i);
+			}
+			else if((i % 5) == 0){
+				System.out.println("Buzz " + i);
+			}
+			
+		}
+	}
+	
+	/**
+	 * Another version of fizzbuzz
+	 */
+	public static void fizzBuzzV2(){
+		
+		for(int i = 1; i <= 100; i++)
+		{
+			if((i % 15) == 0){
+				System.out.println("FizzBuzz " + i);
+			}
+			else if((i % 3) == 0){
+				System.out.println("Fizz " + i);
+			}
+			else if((i % 5) == 0){
+				System.out.println("Buzz " + i);
+			}
+		}
+
+	}
+	
+	/**
+	 * @param args program arguments
+	 */
+	public static void main(String[] args) {
+		
+		fizzBuzzV1();
+		
+		fizzBuzzV2();
+	}
+
+}
